a552c990c238a8646feefe1f69d0a7744351ff0e
Riley Plokhooy <rileyanthony2006@gmail.com>
2025-07-23 11:26:37 -0400

    done

---
+++ Palindrome_Excercise_Test/WordSmithTests.cs
@@ -0,0 +1,30 @@
+﻿using Palindrome_Exercise;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome_Excercise_Test;
+
+public class WordSmithTests
+{
+    [Theory]
+    [InlineData("racecar", true)]
+    [InlineData("Racecar", true)]
+    [InlineData("hannah", true)]
+    [InlineData("kayak", true)]
+    [InlineData("potassium", true)]
+    [InlineData("palindrome", true)]
+    [InlineData("ginger", true)]
+    [InlineData("null", false)]
+    [InlineData("", false)]
+    public void IsAPalindrome_Test(string word, bool expected) 
+    {
+        var ws = new WordSmith();
+
+        var actual = ws.ISAPalindrome(word);
+
+        Assert.Equal(expected, actual);
+    }
+}
---
+++ Palindrome_Exercise/WordSmith.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome_Exercise;
+
+public class WordSmith
+{
+    public bool ISAPalindrome(string word)
+    {
+        if (word == null || word.Length == 0) 
+        {
+            return false;        
+        }
+
+        string wordLowered = word.ToLower();
+        
+        string backwards = "";
+
+        for (int i = wordLowered.Length - 1; i >= 0; i--) 
+        {
+            backwards += wordLowered[i];
+        }
+
+        if (wordLowered == backwards)
+        {
+            return true;
+        }
+        else 
+        {
+            return false;
+        }
+    }
+}
+
